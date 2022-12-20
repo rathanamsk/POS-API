aac73497625f20e91569b5daafff39220445dd23
Saonan19 <thy.saonan19@kit.edu.kh>
2022-12-20 17:47:22 +0700

    crude operation for table

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 const categoy=require("./category")
 const product=require("./product")
+const table=require("./table")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'POS-API' });
 });
 router.use("/",categoy)
 router.use("/",product)
+router.use("/",table)
 
 module.exports = router;
---
+++ src/routes/table.js
@@ -0,0 +1,8 @@
+const table = require('../controller/table');
+const router = require('express').Router()
+router.post('/table', table.createTable)
+router.get('/table', table.getAllTable)
+router.delete('/table/(:id)', table.deleteTable)
+router.put('/table/(:id)', table.updateTable)
+
+module.exports = router;
