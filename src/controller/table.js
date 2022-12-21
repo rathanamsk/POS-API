5c913b247fc9c23d7cde7df0c7770c6dc0a219ce
SeakKimhour <seakkimhor20@kit.edu.kh>
2022-12-21 20:01:26 +0700

    add confirm payment route

--- src/controller/table.js
+++ src/controller/table.js
@@ -24,7 +24,6 @@ module.exports = {
         status: "active",
       });
       if (table_record) return res.status(400).send(response("This table number existed"));
-      console.log("Hello WOrld")
       const newTable = await table.create({
         tableNumber: tableNumber,
         tableName: tableName,
@@ -50,13 +49,12 @@ module.exports = {
     }
   },
 
-    //get all product
-    async getAllAvailableTable(req, res) {
+  //   //get all product
+  async getAllAvailableTable(req, res) {
       try {
         const tables = await table.find({ status: "active" });
         return res.status(200).send(tables);
       } catch (err) {
-        console.log(err);
         return res.status(500).send(response("Fail to get all the products"));
       }
     },
